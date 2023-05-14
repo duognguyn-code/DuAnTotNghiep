be42af8271197509fbdc4e213aa12cc0596ee18d
Dogun11 <77286369+duognguyn-code@users.noreply.github.com>
2023-05-15 00:12:53 +0700

    First commit to DuAnTotNghiep

---
+++ BE_DuAn/src/main/java/com/poly/be_duan/BeDuAnApplication.java
@@ -0,0 +1,13 @@
+package com.poly.be_duan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDuAnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDuAnApplication.class, args);
+    }
+
+}
