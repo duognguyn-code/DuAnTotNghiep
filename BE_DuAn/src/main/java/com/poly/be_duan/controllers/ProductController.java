404284b82f77c4730e2070ef85cab58cb796a94a
Quantran5302 <quantran939@gmail.com>
2023-05-31 16:02:33 +0700

    quan_fe
    quan_fe

--- BE_DuAn/src/main/java/com/poly/be_duan/controllers/ProductController.java
+++ BE_DuAn/src/main/java/com/poly/be_duan/controllers/ProductController.java
@@ -1,4 +1,24 @@
 package com.poly.be_duan.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
+    @GetMapping("/admin/productdetail")
+    public String view(){
+        return "admin/ProductDetail/TabProductDetail";
+    }
+    @GetMapping("/admin/product")
+    public String view22(){
+        return "admin/Product/TabProduct";
+    }
+    @GetMapping("/admin/designs")
+    public String view122(){
+        return "admin/Product/Designs";
+    }
+    @GetMapping("/admin/material")
+    public String view121(){
+        return "admin/Product/material";
+    }
 }
